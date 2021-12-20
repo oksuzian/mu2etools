6ad954bdbeb3b0e7dcc752162d7f0a48c8881f18
oksuzian <48927306+oksuzian@users.noreply.github.com>
2021-12-20 17:58:03 -0600

    more

--- mu2etools/utils.py
+++ mu2etools/utils.py
@@ -3,69 +3,68 @@
 import math
 import pandas
 
-class Utils:
   
-    LIVETIME_LO=1.36E8+5.09E7
-    LIVETIME_HI=5.49E6 
-    LIVETIME_MU2E=3.46E6  
+LIVETIME_LO=1.36E8+5.09E7
+LIVETIME_HI=5.49E6 
+LIVETIME_MU2E=3.46E6  
 
-    def getNormBackground(df, cuts, scale):
-        # scale - to account for a larger momentum window
-        raw_count_hi = len(df.query('is_high==1 & %s' % cuts))
-        raw_count_lo = len(df.query('is_high==0 & %s' % cuts))
+def getNormBackground(df, cuts, scale):
+    # scale - to account for a larger momentum window
+    raw_count_hi = len(df.query('is_high==1 & %s' % cuts))
+    raw_count_lo = len(df.query('is_high==0 & %s' % cuts))
 
-        ave_hi = raw_count_hi/LIVETIME_HI*LIVETIME_MU2E/scale
-        ave_lo = raw_count_lo/LIVETIME_LO*LIVETIME_MU2E/scale
-        ave = ave_hi + ave_lo
+    ave_hi = raw_count_hi/LIVETIME_HI*LIVETIME_MU2E/scale
+    ave_lo = raw_count_lo/LIVETIME_LO*LIVETIME_MU2E/scale
+    ave = ave_hi + ave_lo
   
-        err_hi = math.sqrt(raw_count_hi)/LIVETIME_HI*LIVETIME_MU2E/scale
-        err_lo = math.sqrt(raw_count_lo)/LIVETIME_LO*LIVETIME_MU2E/scale
-        err = math.sqrt(err_hi**2 + err_lo**2)
+    err_hi = math.sqrt(raw_count_hi)/LIVETIME_HI*LIVETIME_MU2E/scale
+    err_lo = math.sqrt(raw_count_lo)/LIVETIME_LO*LIVETIME_MU2E/scale
+    err = math.sqrt(err_hi**2 + err_lo**2)
 
         return (ave, err, ave_hi, ave_lo, err_hi, err_lo)
   
-    def pritnLiveTimes(self):
-        print("LIVETIME_MU2E: %e" % LIVETIME_MU2E)
-        print("LIVETIME_HI: %e" % LIVETIME_HI)
-        print("LIVETIME_LO: %e" % LIVETIME_LO)
+def pritnLiveTimes(self):
+    print("LIVETIME_MU2E: %e" % LIVETIME_MU2E)
+    print("LIVETIME_HI: %e" % LIVETIME_HI)
+    print("LIVETIME_LO: %e" % LIVETIME_LO)
     
-    def getVarList(list_name):
-        list = 'N/A'
-        if list_name=="all":
+def getVarList(list_name):
+    list = 'N/A'
+    if list_name=="all":
             list = {'crvinfo__x': [-8000, 1000], 
-              'crvinfo__y': [0, 3500],
-              'crvinfo__z': [-5000, 20000],
-              'de_nhits': [-2, 100],
-              'ue_nhits': [-2, 100],
-              'dequal_TrkPID': [-1.1, 1.1],
-              'dequal_TrkQual': [-0.1, 1.1],
-              'deent_td': [0, 10],
-              'deent_z0': [-500,500],
-              'deent_d0': [-500,500],
-              'deent_om': [-0.01, 0.05],
-              'crvinfo__PEs': [-1, 1000],
-              'crvinfo__dT': [-500, 500],
-              'deent_mom': [50, 200],
-              'deent_d0_om' : [300, 1200]}
+            'crvinfo__y': [0, 3500],
+            'crvinfo__z': [-5000, 20000],
+            'de_nhits': [-2, 100],
+            'ue_nhits': [-2, 100],
+            'dequal_TrkPID': [-1.1, 1.1],
+            'dequal_TrkQual': [-0.1, 1.1],
+            'deent_td': [0, 10],
+            'deent_z0': [-500,500],
+            'deent_d0': [-500,500],
+            'deent_om': [-0.01, 0.05],
+            'crvinfo__PEs': [-1, 1000],
+            'crvinfo__dT': [-500, 500],
+            'deent_mom': [50, 200],
+            'deent_d0_om' : [300, 1200]}
 
-        if list_name=="withCRV":
+    if list_name=="withCRV":
             list = {'crvinfo__z': [-5000, 20000],
-              'crvinfo__PEs': [-1, 1000],
-              'crvinfo__dT': [-500, 500],
-              'de_nhits': [-2, 100],
-              'ue_nhits': [-2, 100],
-              'dequal_TrkPID': [-1.1, 1.1],
-              'dequal_TrkQual': [-0.1, 1.1],
-              'deent_td': [0, 10],
-              'deent_z0': [-500,500]}
+            'crvinfo__PEs': [-1, 1000],
+            'crvinfo__dT': [-500, 500],
+            'de_nhits': [-2, 100],
+            'ue_nhits': [-2, 100],
+            'dequal_TrkPID': [-1.1, 1.1],
+            'dequal_TrkQual': [-0.1, 1.1],
+            'deent_td': [0, 10],
+            'deent_z0': [-500,500]}
 
-        if list_name=="noCRV":
+   if list_name=="noCRV":
             list = {'de_nhits': [-2, 100],
-              'ue_nhits': [-2, 100],
-              'dequal_TrkPID': [-1.1, 1.1],
-              'dequal_TrkQual': [-0.1, 1.1],
-              'deent_td': [0, 10],
-              'deent_z0': [-500,500]}
+            'ue_nhits': [-2, 100],
+            'dequal_TrkPID': [-1.1, 1.1],
+            'dequal_TrkQual': [-0.1, 1.1],
+            'deent_td': [0, 10],
+            'deent_z0': [-500,500]}
 
     if list_name=="translate":
       list = {'crvinfo__x': "x position", 
