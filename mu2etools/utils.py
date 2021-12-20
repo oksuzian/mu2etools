1c312062c84a501c657166f660fad326eeecc01e
oksuzian <48927306+oksuzian@users.noreply.github.com>
2021-12-20 17:55:11 -0600

    Charge to utils

--- mu2etools/utils.py
+++ mu2etools/utils.py
@@ -5,38 +5,34 @@ import pandas
 
 class Utils:
   
-  def __init__(self):
-    self.LIVETIME_LO=1.36E8+5.09E7
-    self.LIVETIME_HI=5.49E6 
-    self.LIVETIME_MU2E=3.46E6  
+    LIVETIME_LO=1.36E8+5.09E7
+    LIVETIME_HI=5.49E6 
+    LIVETIME_MU2E=3.46E6  
 
-  def getNormBackground(self, df, cuts, scale):
+    def getNormBackground(df, cuts, scale):
+        # scale - to account for a larger momentum window
+        raw_count_hi = len(df.query('is_high==1 & %s' % cuts))
+        raw_count_lo = len(df.query('is_high==0 & %s' % cuts))
 
-  # scale - to account for a larger momentum window 
-
-    raw_count_hi = len(df.query('is_high==1 & %s' % cuts))
-    raw_count_lo = len(df.query('is_high==0 & %s' % cuts))
-
-    ave_hi = raw_count_hi/self.LIVETIME_HI*self.LIVETIME_MU2E/scale
-    ave_lo = raw_count_lo/self.LIVETIME_LO*self.LIVETIME_MU2E/scale
-    ave = ave_hi + ave_lo
+        ave_hi = raw_count_hi/LIVETIME_HI*LIVETIME_MU2E/scale
+        ave_lo = raw_count_lo/LIVETIME_LO*LIVETIME_MU2E/scale
+        ave = ave_hi + ave_lo
   
-    err_hi = math.sqrt(raw_count_hi)/self.LIVETIME_HI*self.LIVETIME_MU2E/scale
-    err_lo = math.sqrt(raw_count_lo)/self.LIVETIME_LO*self.LIVETIME_MU2E/scale
-    err = math.sqrt(err_hi**2 + err_lo**2)
+        err_hi = math.sqrt(raw_count_hi)/LIVETIME_HI*LIVETIME_MU2E/scale
+        err_lo = math.sqrt(raw_count_lo)/LIVETIME_LO*LIVETIME_MU2E/scale
+        err = math.sqrt(err_hi**2 + err_lo**2)
 
-    return (ave, err, ave_hi, ave_lo, err_hi, err_lo)
+        return (ave, err, ave_hi, ave_lo, err_hi, err_lo)
   
-  def pritnLiveTimes(self):
-    print("LIVETIME_MU2E: %e" % self.LIVETIME_MU2E)
-    print("LIVETIME_HI: %e" % self.LIVETIME_HI)
-    print("LIVETIME_LO: %e" % self.LIVETIME_LO)
+    def pritnLiveTimes(self):
+        print("LIVETIME_MU2E: %e" % LIVETIME_MU2E)
+        print("LIVETIME_HI: %e" % LIVETIME_HI)
+        print("LIVETIME_LO: %e" % LIVETIME_LO)
     
-  def getVarList(self, list_name):
-    list = 'N/A'
-    
-    if list_name=="all":
-      list = {'crvinfo__x': [-8000, 1000], 
+    def getVarList(list_name):
+        list = 'N/A'
+        if list_name=="all":
+            list = {'crvinfo__x': [-8000, 1000], 
               'crvinfo__y': [0, 3500],
               'crvinfo__z': [-5000, 20000],
               'de_nhits': [-2, 100],
@@ -52,8 +48,8 @@ class Utils:
               'deent_mom': [50, 200],
               'deent_d0_om' : [300, 1200]}
 
-    if list_name=="withCRV":
-      list = {'crvinfo__z': [-5000, 20000],
+        if list_name=="withCRV":
+            list = {'crvinfo__z': [-5000, 20000],
               'crvinfo__PEs': [-1, 1000],
               'crvinfo__dT': [-500, 500],
               'de_nhits': [-2, 100],
@@ -63,48 +59,13 @@ class Utils:
               'deent_td': [0, 10],
               'deent_z0': [-500,500]}
 
-    if list_name=="noCRV":
-      list = {'de_nhits': [-2, 100],
+        if list_name=="noCRV":
+            list = {'de_nhits': [-2, 100],
               'ue_nhits': [-2, 100],
               'dequal_TrkPID': [-1.1, 1.1],
               'dequal_TrkQual': [-0.1, 1.1],
               'deent_td': [0, 10],
               'deent_z0': [-500,500]}
-
-    if list_name=="outDF":
-      list = {'evtinfo__eventid',
-              'evtinfo__runid',
-              'evtinfo__subrunid',
-              'de_status',
-              'de_pdg',
-              'de_nhits',
-              'de_t0',
-              'deent_mom',
-              'deent_d0',
-              'deent_p0',
-              'deent_om',
-              'deent_z0',
-              'deent_td',
-              'dequal_TrkPID',
-              'dequal_TrkQual',
-              'trigbits',
-              'ue_status',
-              'ue_nhits',
-              'crvinfo__crvSectorType',
-              'crvinfo__x',
-              'crvinfo__y',
-              'crvinfo__z',
-              'crvinfo__PEs',
-              'crvinfo__timeWindowStart',
-              'crvinfo__nCoincidenceHits',
-              'crvinfomc__x',
-              'crvinfomc__y',
-              'crvinfomc__z',
-              'bestcrv',
-              'is_high',
-              'crvinfo__dT',
-              'deent_d0_om',
-              'is_triggered'}
 
     if list_name=="translate":
       list = {'crvinfo__x': "x position", 
