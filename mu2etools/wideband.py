f0013b8ab8a32958b6670c1b06ee9393c09e3aad
Yuri Oksuzian <oksuzian@mu2egpvm05.fnal.gov>
2023-05-31 14:32:08 -0500

    mod wideband.py

--- mu2etools/wideband.py
+++ mu2etools/wideband.py
@@ -2,9 +2,12 @@ import subprocess
 import uproot
 import numpy as np
 
-
-BAD_RUNS=[42, 44, 45, 46, 47, 48, 49, 50, 51, 52, 53, 54, 55, 56, 57, 58, 59, 60, 61, 62, 64, 67, 68, 69, 80, 83, 87, 88, 89, 91, 96, 101, 105, 110, 115, 1027, 1066, 1084, 1134, 1173, 1037, 1038, 1039, 1040, 1041, 1042, 1043, 1173, 1178, 1209, 1374]
-XROOT = False
+BAD_RUNS=[42, 44, 45, 46, 47, 48, 49, 50, 51, 52, 53, 54, 
+          55, 56, 57, 58, 59, 60, 61, 62, 64, 67, 68, 69, 
+          80, 83, 87, 88, 89, 91, 96, 101, 105, 110, 115, 
+          1027, 1066, 1084, 1134, 1173, 1037, 1038, 1039, 
+          1040, 1041, 1042, 1043, 1173, 1178, 1209, 1374,
+          1377, 1378]
 
 class DataProcessor:
     def __init__(self, xroot, fixtimes):
@@ -51,5 +54,5 @@ class DataProcessor:
         filelist = filelist.splitlines()
         return filelist
 
-    def listBadRun(self):
+    def listBadRuns(self):
         print(BAD_RUNS)
