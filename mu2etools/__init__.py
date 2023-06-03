b375ad766d0f896b5f15506e2f2f561167ef0833
Yuri Oksuzian <oksuzian@mu2egpvm05.fnal.gov>
2023-06-03 09:15:41 -0500

    mod wideband.py

--- mu2etools/__init__.py
+++ mu2etools/__init__.py
@@ -1,4 +1,15 @@
 from .utils import getNormBackground, getNormBackground, getNormBackground
+
+import subprocess
+import uproot
+import numpy as np
+
+import matplotlib.pyplot as plt
 import pandas as pd
 import mplhep as hep
 import awkward as ak
+from numpy.core.function_base import linspace
+import dask as dd
+from scipy.optimize import curve_fit
+import uncertainties.unumpy as unumpy
+import nbconvert
